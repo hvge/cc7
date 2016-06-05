5019b1e6e92a694c382b83974155af4b5edf3c00
Juraj Ďurech <durech@avast.com>
2016-06-05 22:22:00 +0200

    Added ObjC helper functions for conversion between NS & CC7/STD objects

---
+++ include/cc7/objc/ObjcHelper.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016 Juraj Durech <durech.juraj@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cc7/ByteArray.h>
+#import <Foundation/Foundation.h>
+
+namespace cc7
+{
+namespace objc
+{
+	/**
+	 Function creates a new NSData object with bytes copied from
+	 given range of bytes.
+	 */
+	NSData * CopyToNSData(const cc7::ByteRange & range);
+	
+	/**
+	 Function creates a new NSString object with UTF8 string
+	 copied from given string.
+	 */
+	NSString * CopyToNSString(const std::string & string);
+	
+	/**
+	 Function creates a new ByteArray object with bytes copied
+	 from given NSData object
+	 */
+	ByteArray CopyFromNSData(NSData * data);
+	
+	/**
+	 Function creates a new std::string with UTF8 string
+	 converted from given NSString object.
+	 */
+	std::string CopyFromNSString(NSString * string);
+	
+} // cc7::objc
+} // cc7
---
+++ src/cc7/platform/apple/ObjcHelper.mm
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 Juraj Durech <durech.juraj@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <cc7/objc/ObjcHelper.h>
+
+namespace cc7
+{
+namespace objc
+{
+	NSData * CopyToNSData(const cc7::ByteRange & range)
+	{
+		return [[NSData alloc] initWithBytes:range.data() length:range.size()];
+	}
+	
+	NSString * CopyToNSString(const std::string & string)
+	{
+		return [[NSString alloc] initWithUTF8String:string.c_str()];
+	}
+	
+	ByteArray CopyFromNSData(NSData * data)
+	{
+		ByteArray result;
+		if (data) {
+			result.assign(ByteRange(data.bytes, data.length));
+		}
+		return result;
+	}
+	
+	std::string CopyFromNSString(NSString * string)
+	{
+		std::string result;
+		if (string) {
+			result.assign(string.UTF8String);
+		}
+		return result;
+	}
+	
+} // cc7::objc
+} // cc7
